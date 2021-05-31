22bf9e0c692ec90c235a2852468eacb47a51e4d0
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2021-05-31 13:52:50 +0100

    Added constructors for testing

--- src/main/java/types/Review.java
+++ src/main/java/types/Review.java
@@ -6,6 +6,12 @@ public class Review {
     private long rating;
     private String comments;
 
+    // Empty constructor for deserializer
+    public Review() { }
+
+    public Review(long rating) {
+        this.rating = rating;
+    }
     public String getName() { return name; }
     public void setName(String value) { this.name = value; }
 
